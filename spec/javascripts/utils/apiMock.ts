3e4372345fddc4a6c9a8f94adef4a8800c5e8607
Piotr P <piotr.palek@housecallpro.com>
2022-10-04 13:26:19 +0200

    add nested import

--- spec/javascripts/utils/apiMock.ts
+++ spec/javascripts/utils/apiMock.ts
@@ -1,3 +1,6 @@
+import otherUtil from './otherUtil';
+
 export default () => {
+  otherUtil();
   console.log('mocking API');
 }
--- src/App.pw.tsx
+++ src/App.pw.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { test, expect } from '@playwright/experimental-ct-react';
-import 'spec-utils/apiMock';
+import 'spec-utils/apiMock.ts';
 import App from './App';
 
 test.use({ viewport: { width: 500, height: 500 } });
