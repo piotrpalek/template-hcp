c0e7ed23354dec8009fda36b9efde9604ae5ddaa
Piotr P <piotr.palek@housecallpro.com>
2022-10-04 10:31:48 +0200

    modify

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -1,17 +1,20 @@
 import type { PlaywrightTestConfig } from '@playwright/experimental-ct-react';
 import { devices } from '@playwright/experimental-ct-react';
+import { resolve } from 'node:path';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './',
-  /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
+  testDir: '',
+  testMatch: ['**/*.pw.tsx'],
+  /* The base directory, relative to the config file,
+  for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
   snapshotDir: './__snapshots__',
   /* Maximum time one test can run for. */
   timeout: 10 * 1000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -24,9 +27,24 @@ const config: PlaywrightTestConfig = {
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
     /* Port to use for Playwright component endpoint. */
     ctPort: 3100,
+    ctViteConfig: {
+      resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+          components: resolve('./src/components'),
+          hooks: resolve('./src/Hooks'),
+          resources: resolve('./src/Resources'),
+          schema: resolve('./src/Schema'),
+          types: resolve('./src/types'),
+          utils: resolve('./src/utils'),
+          spec: resolve('./spec/javascripts'),
+          'spec-utils': resolve('./spec/javascripts/utils'),
+          'spec-resources': resolve('./spec/javascripts/Resources'),
+        },
+      },
+    },
   },
 
   /* Configure projects for major browsers */
@@ -37,18 +55,18 @@ const config: PlaywrightTestConfig = {
         ...devices['Desktop Chrome'],
       },
     },
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
+    // },
   ],
 };
 
--- src/App.pw.tsx
+++ src/App.pw.tsx
@@ -6,5 +6,5 @@ test.use({ viewport: { width: 500, height: 500 } });
 
 test('should work', async ({ mount }) => {
   const component = await mount(<App></App>);
-  await expect(component).toContainText('Learn React');
+  await expect(component).toContainText('Hello world!2!');
 });
