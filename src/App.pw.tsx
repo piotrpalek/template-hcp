0dcfedf07ba9ce12e93ce80b280326f61ca6409e
Piotr P <piotr.palek@housecallpro.com>
2022-10-04 11:30:57 +0200

    broken

--- src/App.pw.tsx
+++ src/App.pw.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { test, expect } from '@playwright/experimental-ct-react';
+import 'spec-utils/apiMock';
 import App from './App';
 
 test.use({ viewport: { width: 500, height: 500 } });
